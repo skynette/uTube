ae803d9cb94908ad8e36a4ddec80c6ffb767f8de
Joshua <cutejosh2@gmail.com>
2023-03-27 02:36:55 +0100

    scaffold for components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
 const App = () => (
-  <>App</>
+	<BrowserRouter>
+		<Box sx={{backgroundColor: "#000"}}>
+			<Navbar/>
+			<Routes>
+				<Route exact path="/" element={<Feed/>} />
+				<Route exact path="/video/:id" element={<VideoDetail/>} />
+				<Route exact path="/channel/:id" element={<ChannelDetail/>} />
+				<Route exact path="/seach/:searchTerm" element={<SearchFeed/>} />
+			</Routes>
+		</Box>
+	</BrowserRouter>
 );
 
 export default App;

cb8383b77feb97f392c0d4673ff7f3a3f8897777
Joshua <cutejosh2@gmail.com>
2023-03-28 09:57:30 +0100

    videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import { Sidebar } from './'
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
 	return (
@@ -10,6 +10,12 @@ const Feed = () => {
 				<Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
 					Copyright 2023 Skynette
 				</Typography>
+			</Box>
+			<Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+				<Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+					New <span style={{ color: '#F31503' }}>Videos</span>
+				</Typography>
+				<Videos/>
 			</Box>
 		</Stack >
 	)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,8 +13,8 @@ const Sidebar = () => (
 					key={category.name}
 					style={{ background: category.name === selectedCategory && '#FC1503', color: 'white' }}
 				>
-					<span>{category.icon}</span>
-					<span>{category.name}</span>
+					<span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
+					<span style={{opacity: category.name === selectedCategory ? '1': '0.8'}}>{category.name}</span>
 				</button>
 			))
 		}

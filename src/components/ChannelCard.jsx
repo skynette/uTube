8bf3676ad0ee78452c564271bc914be81ed6848c
Joshua <cutejosh2@gmail.com>
2023-03-28 17:01:33 +0100

    issues with ChannelCArd

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
-const ChannelCard = () => {
-  return (
-	<div>ChannelCard</div>
-  )
-}
+import { demoProfilePicture } from '../utils/constants'
+
+const ChannelCard = ({ channelDetail }) => (
+	<Box
+		sx={{ boxShadow: 'none', borderRadius: '20px' }}
+	>
+		<Link to={`/channel/${channelDetail?.id?.ChannelId}`}>
+			<CardContent
+				sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#FFF' }}
+			>
+				<CardMedia
+					image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+					alt={channelDetail?.snippet?.title}
+					sx={{ borderRadius: '50%', height: '100%', width: '180px' }}
+				/>
+			</CardContent>
+		</Link>
+	</Box>
+)
+
 
 export default ChannelCard

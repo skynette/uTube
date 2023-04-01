e3eff82e81f16e1d8e3773dc016af50bcc445a6e
Joshua <cutejosh2@gmail.com>
2023-04-01 22:34:59 +0100

    fix channel id bug

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,7 +9,8 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
 		sx={{ boxShadow: 'none', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: { xs: '356px', md: '320px' }, height: '326px', margin: 'auto', marginTop }}
 	>
 		{/* log channel detail */}
-		<Link to={`/channel/${channelDetail?.id}`}>
+		{console.log(channelDetail)}
+		<Link to={`/channel/${channelDetail?.id?.channelId}`}>
 			<CardContent
 				sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#FFF' }}
 			>
